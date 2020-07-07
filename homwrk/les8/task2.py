6b8953037ebd6302171ec422986e2254de247e2c
pandafifs <pandafiiiifs@gmail.com>
2020-07-07 10:21:55 +0300

    task1 and 2 done

--- homwrk/les8/task2.py
+++ homwrk/les8/task2.py
@@ -2,3 +2,23 @@
 # Проверьте его работу на данных, вводимых пользователем.
 # При вводе пользователем нуля в качестве делителя программа
 # должна корректно обработать эту ситуацию и не завершиться с ошибкой.
+
+class No_null_division(Exception):
+    def __init__(self, txt):
+        self.txt = txt
+
+inp_data = input("Введите положительное число: ")
+inp_data_2 = input("Введите положительное число: ")
+
+
+try:
+    inp_data = int(inp_data)
+    inp_data_2 = int(inp_data_2)
+    if inp_data_2 == 0:
+        raise No_null_division("Нельзя делить на ноль")
+except ValueError:
+    print("Вы ввели не число")
+except No_null_division as err:
+    print(err)
+else:
+    print(f'Все хорошо. Ваше число: ({inp_data/inp_data_2})')
