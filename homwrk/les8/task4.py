6d4776e24cf79e694853f1dd879bb0c70d7d2ade
pandafifs <pandafiiiifs@gmail.com>
2020-07-08 09:54:38 +0300

    task4 done

--- homwrk/les8/task4.py
+++ homwrk/les8/task4.py
@@ -4,3 +4,41 @@
 # В базовом классе определить параметры, общие для приведенных типов.
 # В классах-наследниках реализовать параметры, уникальные для каждого типа оргтехники.
 
+class  Warehouse:
+    def __init__(self, *args):
+        self.my_list = []
+
+
+
+class Equipment:
+    def __init__(self, name, price, *args):
+        self.name = name
+        self.price = price
+        self.my_dict = {'название модели': self.name, 'цена за шт.': self.price}
+
+
+    def __str__(self):
+        return f'модель {self.name} цена {self.price}'
+
+
+
+
+class Printer(Equipment):
+    def print (self):
+        return f'я принтер по имени {self.name}'
+
+
+
+class Scaner(Equipment):
+    def print (self):
+        return f'я сканер по имени {self.name}'
+
+
+class Xerox(Equipment):
+    def print (self):
+        return f'я ксерокс по имени {self.name}'
+
+
+a = Scaner('HP', 10000)
+print(a)
+print(a.print())
